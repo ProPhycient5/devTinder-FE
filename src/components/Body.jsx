08bf38651750d26bbd491ed5035eb066ada1a71c
ProPhycient <dsawan.office8@gmail.com>
2026-01-21 10:01:10 +0530

    integrated logout feature

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate, useLocation } from "react-router";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) navigate("/login");
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!userData && location.pathname !== '/login') fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,62 @@
+import axios from "axios";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
 
 const Navbar = () => {
   const userData = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(BASE_URL + "/logout", {}, { withCredentials: true });
+      dispatch(removeUser());
+      navigate("/login");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="navbar bg-base-200 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">🧑‍💻DevTinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          🧑‍💻DevTinder
+        </Link>
       </div>
       {userData && (
         <div className="flex gap-2 item-center justify-center">
-          <div className="flex items-center">Welcome, {userData?.firstName}</div>
+          <div className="flex items-center">
+            Welcome, {userData?.firstName}
+          </div>
           <div className="dropdown dropdown-end">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar mr-5"
-              >
-                <div className="w-10 rounded-full">
-                  <img alt="Profile pic" src={userData?.photoUrl} />
-                </div>
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar mr-5"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="Profile pic" src={userData?.photoUrl} />
               </div>
+            </div>
             <ul
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
               </li>
               <li>
-                <a>Logout</a>
+                <a onClick={handleLogout}>Logout</a>
               </li>
             </ul>
           </div>
